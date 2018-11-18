cab01d73298195ae4b4ae06dfe33df6009a41526
Kennan Smith <kennan.smith1@gmail.com>
2018-11-18 16:46:21 -0700

    built skeleton of main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
-
+import { Row, Column } from 'simple-flexbox'
+import styled from 'styled-components'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello Team Bitch
+        <Column vertical='space-around' horizontal='center'>
+
+          {/* title row */}
+          <Row vertical='center' horizontal='center'>
+
+          </Row>
+
+          {/* main content/k&c profiles */}
+          <Row vertical='center' horizontal='space-around'>
+
+          </Row>
+
+          {/* sidekick profiles */}
+          <Row vertical='center' horizontal='space-around'>
+
+          </Row>
+          
+        </Column>
       </div>
     )
   }
