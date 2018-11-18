c8f12f79c8a02eed039c8aa1039b850d6494d89c
Kennan Smith <kennan.smith1@gmail.com>
2018-11-18 10:33:36 -0700

    initial commit - barebones starter page, made by stripping down starter app from create-react-app command

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello Team Bitch
+      </div>
+    );
+  }
+}
+
+export default App;
