8b29a78d396045fb68e2e267b5d1d443df617f8a
Kennan Smith <kennan.smith1@gmail.com>
2018-11-18 17:24:24 -0700

    added title component directly into App.js via styled-components

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import React, { Component } from 'react'
 import { Row, Column } from 'simple-flexbox'
 import styled from 'styled-components'
 
+let Title = styled.h1`
+  text-align: center;
+  font-size: 5vh;
+  height: 20vh;
+  width: 100vw;
+  padding-top: 10vh;
+  margin-top: 0;
+  background-color: pink;
+`
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
         <Column vertical='space-around' horizontal='center'>
 
           {/* title row */}
           <Row vertical='center' horizontal='center'>
-
+            <Title>Team Bitch</Title>
           </Row>
 
           {/* main content/k&c profiles */}
@@ -22,9 +31,8 @@ class App extends Component {
           <Row vertical='center' horizontal='space-around'>
 
           </Row>
-          
+
         </Column>
-      </div>
     )
   }
 }
